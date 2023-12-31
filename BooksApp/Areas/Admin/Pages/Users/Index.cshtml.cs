d31286ced57cdb07364c7594d7b3b455393da1ec
Oskar <oskar.smilek@microsoft.wsei.edu.pl>
2023-12-31 13:33:17 +0100

    Add delete action confirmation to user list page

--- BooksApp/Areas/Admin/Pages/Users/Index.cshtml.cs
+++ BooksApp/Areas/Admin/Pages/Users/Index.cshtml.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿#nullable disable
+
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
